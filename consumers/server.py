f70cc2ae809008c7b99b7770dded3aa28d0f115f
kevinbaumer <k.baumer@celonis.com>
2021-01-24 14:02:48 +0100

    modified server.py

--- consumers/server.py
+++ consumers/server.py
@@ -74,7 +74,7 @@ def run_server():
             is_avro=False,
         ),
         KafkaConsumer(
-            "^com.udacity.stations.arrivalsnew2.",
+            "^com.udacity.stations.arrivalsnew2.*",
             lines.process_message,
             offset_earliest=True,
         ),
